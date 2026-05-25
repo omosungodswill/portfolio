2859b02ab7054937a2bf344dc2ecbd58437ced94
omosungodswill <omosun.godswill@gmail.com>
2026-05-25 10:25:03 +0100

    portfolio 1st attempt

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleBackground() {
+  if (document.body.style.backgroundColor === "black") {
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.body.style.backgroundColor = "black";
+  }
+}
